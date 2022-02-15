309192be523297e6e38b7fc8222ae531dc98567d
= <i.acnaylor@gmail.com>
2022-02-15 16:24:51 +0000

    fixed broken things/ignored them

--- app/db.py
+++ app/db.py
@@ -16,24 +16,21 @@ def get_db() -> sql.Connection | Any:
 
     return g.db
 
-
-def close_db(e: Optional[BaseException] | None) -> None:
+def close_db(_: Optional[BaseException] | None) -> None:
     db = g.pop('db', None)
     if db is not None:
         db.close()
 
-
 def init_db() -> None:
     db = get_db()
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+    with current_app.open_resource('schema.sql', mode='r') as f:
+        db.executescript(f.read())
 
 @click.command('init-db')
-@with_appcontext
+@with_appcontext # type: ignore
 def init_db_command() -> None:
     init_db()
     click.echo("Initialised Database")
-
 
 def init_app(app: Flask) -> None:
     app.teardown_appcontext(close_db)
