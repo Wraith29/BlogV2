cff42a341de7e2d9889724a655028ca54f69282b
Isaac Naylor <i.acnaylor@gmail.com>
2022-02-19 12:52:25 +0000

    cleaned up

--- app/routes/auth/register.py
+++ app/routes/auth/register.py
@@ -1,12 +1,12 @@
 from typing import Tuple
 
-from flask import (Blueprint,Response ,redirect,
+from flask import (Blueprint, Response ,redirect,
                     render_template, request, url_for)
 from werkzeug.security import generate_password_hash
 
 from app.db import get_db
 from app.models import User
-from app.utilities import flash_and_redirect, get_user_by_username, is_valid_username
+from app.utilities import create_user, flash_and_redirect, get_user_by_username, is_valid_username
 
 register_bp = Blueprint("register", __name__, url_prefix="/register")
 
@@ -19,7 +19,8 @@ def view() -> Tuple[str, int]:
 
 @register_bp.route("/register", methods=["POST"])
 def register() -> Tuple[Response, int]:
-    user_exists = get_user_by_username(get_db(), request.form)
+    db = get_db()
+    user_exists = get_user_by_username(db, request.form)
     if user_exists:
         return flash_and_redirect(('Username Taken', 'error'), 'auth.login.view', 301)
 
@@ -27,6 +28,7 @@ def register() -> Tuple[Response, int]:
         return flash_and_redirect(('Invalid Username', 'error'), 'auth.register.view', 301)
     
     new_user = User(None, request.form['username'], generate_password_hash(request.form['password']))
+    id = create_user(db, new_user)
 
     return redirect(url_for("view.home.home")), 200
 
