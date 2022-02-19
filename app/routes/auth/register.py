a2ff74542f65db8ded75782483f9d90ba8175ae5
Isaac Naylor <i.acnaylor@gmail.com>
2022-02-19 12:16:03 +0000

    started adding registration

--- app/routes/auth/register.py
+++ app/routes/auth/register.py
@@ -1,10 +1,12 @@
 from typing import Tuple
 
-from flask import (Blueprint,Response,flash,redirect,
-                    render_template,request,url_for)
+from flask import (Blueprint,Response ,redirect,
+                    render_template, request, url_for)
+from werkzeug.security import generate_password_hash
 
 from app.db import get_db
-from app.utilities import flash_and_redirect, get_user_by_username
+from app.models import User
+from app.utilities import flash_and_redirect, get_user_by_username, is_valid_username
 
 register_bp = Blueprint("register", __name__, url_prefix="/register")
 
@@ -21,7 +23,11 @@ def register() -> Tuple[Response, int]:
     if user_exists:
         return flash_and_redirect(('Username Taken', 'error'), 'auth.login.view', 301)
 
-    # TODO: add register stuff
+    if not is_valid_username(request.form['username']):
+        return flash_and_redirect(('Invalid Username', 'error'), 'auth.register.view', 301)
+    
+    new_user = User(None, request.form['username'], generate_password_hash(request.form['password']))
+
     return redirect(url_for("view.home.home")), 200
 
 __all__ = ["register_bp"]
