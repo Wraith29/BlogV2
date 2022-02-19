a1f200f280088b89a243b77e1908b899244abbe2
Isaac Naylor <i.acnaylor@gmail.com>
2022-02-19 12:15:21 +0000

    added username validation method

--- app/utilities.py
+++ app/utilities.py
@@ -1,4 +1,5 @@
 import sqlite3 as sql
+import string
 from typing import Dict, List, Any, Tuple
 
 from flask import Response, flash, redirect, url_for
@@ -18,6 +19,20 @@ def get_posts_by_user_id(db: sql.Connection, user_id: int) -> List[Post] | List[
     posts = db.execute(PostQueries.GetPostsByAuthorId, [user_id]).fetchall()
     return [Post(*post) for post in posts] if posts else []
 
+def is_valid_username(username: str) -> bool:
+    for c in username:
+        if c not in [*string.ascii_letters, *string.digits]:
+            return False
+    return True 
+
 def flash_and_redirect(flash_details: Tuple[str, str], redirect_route: str, status_code: int) -> Tuple[Response, int]:
     flash(*flash_details)
     return redirect(url_for(redirect_route)), status_code
+
+__all__ = [
+    "get_user_by_username",
+    "get_user_by_id",
+    "get_posts_by_user_id",
+    "is_valid_username",
+    "flash_and_redirect"
+]
