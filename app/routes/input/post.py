e782cc89e9662d0477837737a2b15b85e8f1b4f6
Isaac Naylor <i.acnaylor@gmail.com>
2022-02-21 19:05:19 +0000

    created basic post routes

---
+++ app/routes/input/post.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+from flask import Blueprint, Response, redirect, render_template, url_for
+
+
+post_bp = Blueprint('post', __name__, url_prefix='/post')
+
+@post_bp.get('/')
+def view() -> Tuple[str, int]:
+    return render_template('input/post.html'), 200
+
+@post_bp.post('/create')
+def create() -> Response:
+    return redirect(url_for('view.home.home'))
