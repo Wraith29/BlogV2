0a51f428ec24876f66aa57451cb9f44014f18706
Isaac Naylor <i.acnaylor@gmail.com>
2022-02-21 11:34:38 +0000

    added username to navbar when logged in

--- app/static/scripts/navbar.js
+++ app/static/scripts/navbar.js
@@ -1,25 +1,17 @@
-// let nav_list = document.querySelector('ul#navbar-items');
-
-// let links = [
-//     '/',
-//     '/profile'
-// ]
-
-// if (!links.includes(window.location.pathname)) {
-//     for (let list_item of nav_list.children) {
-//         let link = list_item.querySelector('a.nav-link');
-//         link.classList.remove('active');
-//         sessionStorage.setItem('active_link', null);
-//     }
-// }
-
 const navbar = document.querySelector('nav#navbar');
+
+const links = [
+    '/', '/profile/all', '/auth/login/', '/auth/register/'
+]
 
 for ( let child of navbar.children ) {
     for ( let sub_child of child.children ) {
-        console.log(sub_child);
-        console.log(sub_child.querySelector('a.nav-link'));
         let link = sub_child.querySelector('a.nav-link');
+        if ( !links.includes(window.location.pathname) ) {
+            link.classList.remove('active');
+            sessionStorage.setItem('active_link', null);
+        }
+
         if ( sessionStorage.getItem('active_link') === link.id ) {
             link.classList.add('active');
         } else {
